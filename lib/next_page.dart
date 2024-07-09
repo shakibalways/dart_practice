210ba67e3489c89df3a9632f5f723b837967d4e7
S <shakibmohammad18@gmail.com>
2024-07-09 23:04:13 +0600

    Date Time

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,32 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// class NextPage extends StatelessWidget {
-//   List<String> colorSet = [
-//     "Red",
-//     "Yellow",
-//     'Pink,'
-//         'Green',
-//     'blue' "Red",
-//     "Yellow",
-//     'Pink,'
-//   ];
-//   NextPage({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: colorSet
-//             .map((e) => Center(
-//               child: Card(
-//                         color: Colors.yellow,
-//                     child:
-//                     Text(e),
-//                   ),
-//             ))
-//             .toList(),
-//       ),
-//     );
-//   }
-// }
+
+
+
