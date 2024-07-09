0c5581771c2c6ce52cf44e83d074fcd4acb38942
S <shakibmohammad18@gmail.com>
2024-07-09 23:29:22 +0600

    Map to list convert

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 
+// void main(){
+//   String welcome ="sakib noor faruk raju abid robiul";
+// var list =welcome.split('  ');
+// print(list.runtimeType);
+// var a= list.reversed;
+// print(a);
+// var b=a.join();
+// print(b);
+//
+//
+// }
+// void main(){
+// Map map ={
+//   'name':'kuddus',
+//   'roll':'01',
+//   'dep':'CSE',
+//   'shift':'1st'
+//
+// };
+//  var a= map.entries.map((e) => e.value).toList();
+// print(a);
+//
+// }
+import 'package:dart_partice/next_page.dart';
+
 void main(){
-  int year =2000;
-  var birthYears = DateTime.now().year-year;
-  print(birthYears);
+Practice of = Practice();
+of.function();
+
+
+int year = 2000;
+var birthYears = DateTime.now().year - year;
+print(birthYears);
+
+
+
+
+
+
 
 }
--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,3 +1,27 @@
+class Practice{
+  void function(){
+    Map map ={
+      'name':'shakib',
+      'roll':'501886',
+      'dep':'CSE',
+      'shift':'1st',
+    };
+    map['omg']='yes3';
+    map['omg1']='yes1';
+    map['omg2']='2';
+    map['omg3']='yes2';
+    var a= map.entries.map((e) => e.value).toList();
 
+    print(a);
+    List l =['shakib'];
+    l.runtimeType;
+
+    var b = l.asMap();
+    l.join();
+    print(b);
+
+  }
+
+}
 
 
