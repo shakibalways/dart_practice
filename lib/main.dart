ca363291335a6d4634ea20682b2f61b542d60774
S <shakibmohammad18@gmail.com>
2024-07-09 19:24:17 +0600

    String convert list and list in reversed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
-
-
-
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'next_page.dart';
+//
+// void main() {
+//
+//  runApp(MyApp());
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//      home: NextPage(),
+//     );
+//   }
+// }
 void main(){
- Map map1= {
- };
+  String welcome ="sakib noor faruk raju abid robiul";
+var list =welcome.split('  ');
+  print(list.runtimeType);
+var a= list.reversed;
+print(a);
+var b=a.join();
+print(b);
 
- List details =['a','b','c','d'];
-
- var map = details.asMap();
 
 
 
- print(map);
+
+
 }
